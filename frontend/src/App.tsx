009e5dc2e3498deda98ed6de55b5702d3c673296
Fard-Faru <fardin2237faruk@gmail.com>
2024-06-09 15:27:12 -0400

    Backend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // Checking backend connection
+  useEffect(() => {
+    fetch('http://localhost:5000/api')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
 
   return (
     <>
