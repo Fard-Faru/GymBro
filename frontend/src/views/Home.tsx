b5bb1880f0a873a8f036207a6f98258a0f2da92a
Ajinkya-B <ajinkya.bhosale85@gmail.com>
2024-06-14 18:27:06 -0400

    Added the base structure + routing for the app

---
+++ frontend/src/views/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">Home page content</p>
+    </div>
+  );
+}
---
+++ frontend/src/views/Signup.tsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export default function Signup() {
+  return (
+    <div className="text-3xl font-bold underline">Signup page content</div>
+  );
+}
