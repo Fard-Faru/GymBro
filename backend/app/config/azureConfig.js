3a6010ab19e3f2fdaf04b9a9bb6d5f83ed3351b7
Fard-Faru <fardin2273faruk@gmail.com>
2024-06-28 16:00:08 -0400

    sql connection established and can be seen in frontend

---
+++ backend/app/config/azureConfig.js
@@ -0,0 +1,27 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+
+const mssql = require('mssql');
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: true
+    }
+}
+
+const connection = mssql.connect(dbConfig)
+    .then(pool => {
+        console.log("Connected to Azure DB Successfully");
+        return pool
+    })
+    .catch(err => {
+        console.error('DB Connection Failed! Erro: ', err);
+        throw err;
+    })
+
+module.exports = connection;
